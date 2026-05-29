d84b7ae8ee0317e8716050e92f2d66a632f506fc
Davi Reis <davireispinheiro2008@gmail.com>
2026-05-29 19:54:32 -0300

    Create Arithmetic mean.py

---
+++ Arithmetic mean.py
@@ -0,0 +1,8 @@
+MA1 = float(input('Digite a 1 nota: ')) 
+MA2 = float(input( 'Digite a 2 nota: '))
+MA3 = float(input('Digite a 3 nota: '))
+MA4 = float(input('Digite a 4 nota: ')) 
+
+MAR = (MA1 + MA2 + MA3 + MA4) / 4
+
+print('média aritemétcia é:', MAR) 
