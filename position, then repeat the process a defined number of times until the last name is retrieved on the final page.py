30dd96c8c7ffbfb99cfd2d94a36b74c4356d8c51
Davi Reis <davireispinheiro2008@gmail.com>
2026-05-30 13:00:56 -0300

    Add files via upload

---
+++ position, then repeat the process a defined number of times until the last name is retrieved on the final page.py
@@ -0,0 +1,28 @@
+#Import URL request. Open a web page and select a Link (Name)/position, then repeat the process a defined number of times until the last name is retrieved on the final page.
+
+import urllib.request
+from bs4 import BeautifulSoup
+
+# Input values
+url = input("Enter URL: ")
+count = int(input("Enter count: "))
+position = int(input("Enter position: "))
+
+# Repeat the process count + 1 times
+for i in range(count):
+    print("Retrieving:", url)
+
+    html = urllib.request.urlopen(url).read()
+    soup = BeautifulSoup(html, "html.parser")
+
+    # Find all anchor tags
+    tags = soup('a')
+
+    # Get the link at the required position
+    target = tags[position - 1]
+    url = target.get('href')
+
+    # Save the last name found
+    last_name = target.text
+
+print("Last name:", last_name)
